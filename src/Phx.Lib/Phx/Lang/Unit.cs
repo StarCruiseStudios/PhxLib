9913b092bb63f2bff80fd4b718196ff2d2e0e443
Dangerdan9631 <dangerdan9631@live.com>
2022-09-29 00:25:40 -0700

    Fix license text

--- src/Phx.Lib/Phx/Lang/Unit.cs
+++ src/Phx.Lib/Phx/Lang/Unit.cs
@@ -1,7 +1,7 @@
 // -----------------------------------------------------------------------------
 //  <copyright file="Unit.cs" company="Star Cruise Studios LLC">
 //      Copyright (c) 2022 Star Cruise Studios LLC. All rights reserved.
-//      Licensed under the Apache License 2.0 License.
+//      Licensed under the Apache License, Version 2.0.
 //      See http://www.apache.org/licenses/LICENSE-2.0 for full license information.
 //  </copyright>
 // -----------------------------------------------------------------------------
@@ -9,10 +9,8 @@
 namespace Phx.Lang {
     using System;
 
+    /// <summary> A type that represents no type and holds no information. </summary>
     [Serializable]
-    /// <summary>
-    ///     A type that represents no type and holds no information.
-    /// </summary>
     public sealed class Unit {
         /// <summary> The <see cref="Unit" /> instance. </summary>
         public static Unit UNIT = new();
--- src/Phx.Lib/README.md
+++ src/Phx.Lib/README.md
@@ -3,5 +3,5 @@
 PHX Lib core utilities and extensions.
 
 Copyright (c) 2022 Star Cruise Studios LLC. All rights reserved. 
-Licensed under the Apache License 2.0 License. 
+Licensed under the Apache License, Version 2.0. 
 See http://www.apache.org/licenses/LICENSE-2.0 for full license information.
